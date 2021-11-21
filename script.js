97877f7f18840d7a75e1d42ae1c583d0865af7fb
abdurakhmon <abdurakhmon@gmail.com>
2021-11-21 11:27:26 +0500

    calculation

---
+++ script.js
@@ -0,0 +1,26 @@
+var srt = prompt('Введите свое имя');
+console.log(srt);
+var srt = prompt('Введите свое фамилию');
+console.log(srt);
+var srt = prompt('Введите свой возраст');
+console.log(srt);
+
+var srt = +prompt('5 + 5 = ?' )
+console.log(srt);
+
+console.log( '5 + 5 = ' + ( 5 + 5 ) + ' Ваш ответ: 10' );
+
+var srt = +prompt('5 - 5 = ?')
+console.log(srt);
+
+console.log('5 - 5 = ' + ( 5 - 5 ) + ' Ваш ответ: 0');
+
+var srt = +prompt('5/5=?')
+console.log(srt);
+
+console.log('5 / 5 = ' + ( 5 / 5 ) + ' Ваш ответ: 1');
+
+var srt = +prompt('10%5=?')
+console.log(srt);
+
+console.log('10  % 5 = ' + ( 10 % 5 ) + ' Ваш ответ: 0');
